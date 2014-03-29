4177c03fa81d3afe15eff213e8288fcdc31da6dc
AlanKong <lingkong@umich.edu>
2014-03-29 13:40:56 -0400

    alan update 1

---
+++ Assets/Scripts/TileMakeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMakeScript : MonoBehaviour {
+
+	public GameObject waterTile;
+	public GameObject grassTile;
+	public GameObject hillTile;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
