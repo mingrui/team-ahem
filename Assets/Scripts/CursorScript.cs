4f606255c3b84f1b29696dae570211f36e2e58af
AlanKong <lingkong@umich.edu>
2014-03-29 21:10:27 -0400

    osodjosd

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour {
+
+	public static Vector3 pos = new Vector3 (0f, 5f, 0f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		gameObject.transform.position = pos;
+	
+	}
+}
