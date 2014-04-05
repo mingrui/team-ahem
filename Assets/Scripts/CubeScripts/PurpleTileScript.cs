8e770010052ba5dc739fb98b30fb42bbb5bbc5e6
AlanKong <lingkong@umich.edu>
2014-04-05 18:08:53 -0400

    Alan Mini-Update
    - Changed Timer GUI Icon
    - Changed Minion Sprite (Yellow & Purple)
    - Tiles will now be "highlighted" when moused over
    - Minion will now be "boxed" when moused over

--- Assets/Scripts/CubeScripts/PurpleTileScript.cs
+++ Assets/Scripts/CubeScripts/PurpleTileScript.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class PurpleTileScript : MonoBehaviour {
 
 	public Texture grass;
+	public Texture grassLight;
+
+	public Texture purple;
+	public Texture purpleLight;
+
+	public bool bananaGone = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +25,12 @@ public class PurpleTileScript : MonoBehaviour {
 		if (minion.tag == "Player") {
 						minion.GetComponent<MovementControl> ().changeSpeed (-1f);
 			minion.GetComponentInChildren<TurnPurpleScript>().turnPurple();
+			minion.GetComponent<MovementControl>().isPurple = true;
 
 						renderer.material.mainTexture = grass;
 			Vector3 size = new Vector3 (1f, 1f, 1f);
 			GetComponent<BoxCollider> ().size = size;
+			bananaGone = true;
 				}
 	}
 
@@ -30,6 +38,24 @@ public class PurpleTileScript : MonoBehaviour {
 				renderer.material.mainTexture = grass;
 				Vector3 size = new Vector3 (1f, 1f, 1f);
 				GetComponent<BoxCollider> ().size = size;
+		bananaGone = true;
 		}
+
+	void OnMouseEnter() {
+		if (bananaGone == false) {
+						renderer.material.mainTexture = purpleLight;
+				} else {
+						renderer.material.mainTexture = grassLight;
+				}
+	}
+	
+	void OnMouseExit() {
+		if (bananaGone == false) {
+						renderer.material.mainTexture = purple;
+				}
+		else {
+			renderer.material.mainTexture = grass;
+		}
+	}
 		
 	}
