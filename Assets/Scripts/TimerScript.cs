ff293b249aa02bca07887a393cdc0f75c4fb845b
hongboso <hongboso@umich.edu>
2014-04-04 21:50:44 -0400

    gui added

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+	public static float timeRemaining = 60f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+	}
+
+	/*
+	void OnGUI () {
+
+		if (timeRemaining > 0) {
+			string timeLeft = timeRemaining.ToString();
+			GUI.Button(new Rect (10,10,100,60), timeLeft);
+		} else if (timeRemaining > -3){
+			GUI.Button(new Rect (10,10,100,60), "Times Up");
+		}
+	}
+	*/
+}
