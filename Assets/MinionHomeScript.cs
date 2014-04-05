3d058e68e1ee4d80e920d092d95101baad01acfd
AlanKong <lingkong@umich.edu>
2014-04-04 21:13:23 -0400

    alan GUI update
    aaaaa

---
+++ Assets/MinionHomeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionHomeScript : MonoBehaviour {
+
+	public static int minionCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider minion){
+		if (minion.tag == "Player") {
+			minionCount ++;
+		}
+	}
+}
