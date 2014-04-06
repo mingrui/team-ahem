05a932ec04a474ad16521e9ed72497477f3f629e
Mingrui Jiang <mjray369@gmail.com>
2014-04-06 09:44:45 -0400

    commit from rollback

--- Assets/Scripts/MinionScripts/MovementControl.cs
+++ Assets/Scripts/MinionScripts/MovementControl.cs
@@ -78,7 +78,7 @@ public class MovementControl : MonoBehaviour {
 
 	// click on this to command this to move in another direction is stopped
 	void OnMouseDown() {
-        Debug.Log("clicked");
+        //Debug.Log("clicked");
         // can only click when not moving
         if(rigidbody.velocity != new Vector3(0, 0, 0)){
             //Debug.Log("moving!");
