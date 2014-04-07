dc733026d6bd00475bc04bc5279523c51409eba7
AlanKong <lingkong@umich.edu>
2014-04-07 19:18:21 -0400

    Alan Update: New GUI Element
    - GUI will now display how many Minions are "stuck"

---
+++ Assets/Scripts/2DGuiScripts/StoppedTextScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoppedTextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "Minions Stuck: " + StopDetectionScript.minionStopCount.ToString ();
+	
+	}
+}
