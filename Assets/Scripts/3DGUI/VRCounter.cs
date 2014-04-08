97acf132978ba242e29fdabed7feae1e4a863998
Mingrui Jiang <mjray369@gmail.com>
2014-04-07 20:56:58 -0400

    Oculus 3D GUI now working.

---
+++ Assets/Scripts/3DGUI/VRCounter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRCounter : MonoBehaviour {
+
+    public UILabel label;
+
+    void Update()
+    {
+        label.text = "Minions Saved: " + MinionHomeScript.minionCount.ToString();
+
+    }
+}
---
+++ Assets/Scripts/3DGUI/VRTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRTimer : MonoBehaviour {
+    public UILabel label;
+
+    void Update()
+    {
+        float timeR = (int)(TimerScript.timeRemaining);
+        if (timeR > 0)
+        {
+            label.text = "Time Remaining: " + timeR.ToString();
+        }
+        else
+        {
+            label.text = "Times Up ";
+            Time.timeScale = 0;
+        }
+
+    }
+}
