5054a002b12cab0c37dabd4992bb9b8c9f81e58d
Mingrui Jiang <mjray369@gmail.com>
2014-04-22 13:47:23 -0400

    Get 3D ready for the presentation

---
+++ Assets/Scripts/3DGUI/VRMinionStuck.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRMinionStuck : MonoBehaviour {
+
+    public UILabel label;
+
+    void Update()
+    {
+        label.text = "Minions Stuck: " + StopDetectionScript.minionStopCount.ToString();
+
+    }
+}
---
+++ Assets/Scripts/3DGUI/VRScoreLabel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRScoreLabel : MonoBehaviour {
+
+    public UILabel label;
+
+    void Update()
+    {
+        label.text = "Score: " + StopDetectionScript.score.ToString();
+
+    }
+}
